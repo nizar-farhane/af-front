61ed4318cc6b84b20aeebc4ccc89fcc3a82e7de2
nizar-farhane <106151086+nizar-farhane@users.noreply.github.com>
2022-06-20 13:59:19 +0200

    Create Back.php

---
+++ Back.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr-FR" prefix="og: https://ogp.me/ns#">
+<head>
+<title>EnVols, le media de l’évasion.</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<link rel="alternate" hreflang="en" href="https://www.en-vols.com/en/" />
+<link rel="alternate" hreflang="fr" href="https://www.en-vols.com/" />
+<link rel="alternate" hreflang="x-default" href="https://www.en-vols.com/" />
+
+<meta name="description" content="EnVols, le media de l’évasion. Inspiration, culture, gastronomie, style, vidéos, podcasts… Retrouvez toutes les dernières tendances et les meilleures adresses sélectionnées dans le monde sur EnVols." />
+<meta name="robots" content="follow, index" />
+<link rel="canonical" href="https://www.en-vols.com/" />
+<link rel="next" href="https://www.en-vols.com/page/2/" />
+<meta property="og:locale" content="fr_FR" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="EnVols, le media de l’évasion." />
+<meta property="og:description" content="EnVols, le media de l’évasion." />
+<meta property="og:url" content="https://www.en-vols.com/" />
+<meta property="og:site_name" content="EnVols" />
+<meta property="og:image" content="https://www.en-vols.com/wp-content/uploads/afmm/2022/02/1645468219848-1024x683.jpeg" />
+<meta property="og:image:secure_url" content="https://www.en-vols.com/wp-content/uploads/afmm/2022/02/1645468219848-1024x683.jpeg" />
+<meta property="og:image:width" content="1024" />
+<meta property="og:image:height" content="683" />
+<meta property="og:image:type" content="image/jpeg" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:title" content="EnVols, le media de l’évasion." />
+<meta name="twitter:description" content="EnVols, le media de l’évasion." />
+<meta name="twitter:image" content="https://www.en-vols.com/wp-content/uploads/afmm/2022/02/1645468219848-1024x683.jpeg" />
+<script type="application/ld+json" class="rank-math-schema">{"@context":"https://schema.org","@graph":[{"@type":"Airline","@id":"https://www.en-vols.com/#organization","name":"En-vols.com","url":"https://en-vols.com"},{"@type":"WebSite","@id":"https://www.en-vols.com/#website","url":"https://www.en-vols.com","name":"En-vols.com","publisher":{"@id":"https://www.en-vols.com/#organization"},"inLanguage":"fr-FR","potentialAction":{"@type":"SearchAction","target":"https://www.en-vols.com/?s={search_term_string}","query-input":"required name=search_term_string"}},{"@type":"CollectionPage","@id":"https://www.en-vols.com/#webpage","url":"https://www.en-vols.com/","name":"EnVols, le media de l\u2019\u00e9vasion.","about":{"@id":"https://www.en-vols.com/#organization"},"isPartOf":{"@id":"https://www.en-vols.com/#website"},"inLanguage":"fr-FR"}]}</script>
+<meta name="google-site-verification" content="HpaGY3KvAhH8Eu6h5ZBxFE_873XSbcMJOGMA_gcqqCI" />
+
+<script type="text/javascript">window.gdprAppliesGlobally=true;(function(){function a(e){if(!window.frames[e]){if(document.body&&document.body.firstChild){var t=document.body;var n=document.createElement("iframe");n.style.display="none";n.name=e;n.title=e;t.insertBefore(n,t.firstChild)}
+        else{setTimeout(function(){a(e)},5)}}}function e(n,r,o,c,s){function e(e,t,n,a){if(typeof n!=="function"){return}if(!window[r]){window[r]=[]}var i=false;if(s){i=s(e,t,n)}if(!i){window[r].push({command:e,parameter:t,callback:n,version:a})}}e.stub=true;function t(a){if(!window[n]||window[n].stub!==true){return}if(!a.data){return}
+        var i=typeof a.data==="string";var e;try{e=i?JSON.parse(a.data):a.data}catch(t){return}if(e[o]){var r=e[o];windown}}
+        if(typeof window[n]!=="function"){window[n]=e;if(window.addEventListener){window.addEventListener("message",t,false)}else{window.attachEvent("onmessage",t)}}}e("tcfapi","tcfapiBuffer","tcfapiCall","tcfapiReturn");a("__tcfapiLocator");(function(e){
+          var t=document.createElement("script");t.id="spcloader";t.type="text/javascript";t.async=true;t.src="https://sdk.privacy-center.org/"+e+"/loader.js?target="+document.location.hostname;t.charset="utf-8";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n)})("745a135f-908d-4bfb-beca-56b93e25dc45")})();</script>
+<script type="didomi/javascript" data-vendor="iab:631">(function(a,d,e,b,f,c,s){a[b]=a[b]||function(){a[b].q.push(arguments);};
+			a[b].q=[];c=d.createElement(e);c.async=1;c.src="//tdn.r42tag.com/lib/"+f+".js";
+			s=d.getElementsByTagName(e)[0];s.parentNode.insertBefore(c,s);})
+			(window,document,"script","_st", "1205-v1");
+			// Add additional settings here (optional)
+			_st('setPageStructure', 'reworld', 'page-home' ); 
+			_st('addTagProperties',{"Country":"IE"});
+			_st("loadTags");</script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-219112111-1"></script>
+<script>
+                window.dataLayer = window.dataLayer || [];
+
+                function gtag() {
+                    dataLayer.push(arguments);
+                }
+                gtag('js', new Date());
+
+                gtag('config', 'UA-219112111-1' );
+            </script> 
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-192639368-1"></script>
